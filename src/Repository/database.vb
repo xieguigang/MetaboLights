334563aeedf2e0f382105e4946f457a6cd5286cf
guigang xie <gg.xie@bionovogene.com>
2022-03-25 15:11:55 +0800

    split source file

--- src/Repository/database.vb
+++ src/Repository/database.vb
@@ -1,4 +1,4 @@
-﻿Imports System.Xml.Serialization
+﻿Imports System.Runtime.CompilerServices
 Imports Microsoft.VisualBasic.Text.Xml.Linq
 
 Public Class database
@@ -10,63 +10,14 @@ Public Class database
     Public Property entry_count As Integer
     Public Property entries As entry()
 
+    <MethodImpl(MethodImplOptions.AggressiveInlining)>
     Public Overrides Function ToString() As String
         Return description
     End Function
 
+    <MethodImpl(MethodImplOptions.AggressiveInlining)>
     Public Shared Function LoadReferenceEntries(file As String) As IEnumerable(Of entry)
         Return file.LoadUltraLargeXMLDataSet(Of entry)()
     End Function
 
 End Class
-
-Public Class entry
-
-    <XmlAttribute> Public Property id As String
-    Public Property name As String
-    Public Property description As String
-    Public Property cross_references As ref()
-    Public Property dates As [date]()
-    Public Property additional_fields As field()
-
-    Public Overrides Function ToString() As String
-        Return $"[{id}] {name}"
-    End Function
-
-End Class
-
-Public Class ref
-
-    <XmlAttribute> Public Property dbkey As String
-    <XmlAttribute> Public Property dbname As String
-
-    Public Overrides Function ToString() As String
-        Return $"{dbname}: {dbkey}"
-    End Function
-
-End Class
-
-Public Class [date]
-
-    <XmlAttribute> Public Property type As String
-    <XmlAttribute> Public Property value As String
-
-    Public Overrides Function ToString() As String
-        Return $"{type}: {value}"
-    End Function
-
-End Class
-
-Public Class field
-
-    <XmlAttribute>
-    Public Property name As String
-
-    <XmlText> Public Property value As String
-
-    Public Overrides Function ToString() As String
-        Return $"{name}: {value}"
-    End Function
-
-End Class
-
---
+++ src/Repository/entry.vb
@@ -0,0 +1,57 @@
+﻿Imports System.Runtime.CompilerServices
+Imports System.Xml.Serialization
+
+Public Class entry
+
+    <XmlAttribute> Public Property id As String
+    Public Property name As String
+    Public Property description As String
+    Public Property cross_references As ref()
+    Public Property dates As [date]()
+    Public Property additional_fields As field()
+
+    <MethodImpl(MethodImplOptions.AggressiveInlining)>
+    Public Overrides Function ToString() As String
+        Return $"[{id}] {name}"
+    End Function
+
+End Class
+
+Public Class ref
+
+    <XmlAttribute> Public Property dbkey As String
+    <XmlAttribute> Public Property dbname As String
+
+    <MethodImpl(MethodImplOptions.AggressiveInlining)>
+    Public Overrides Function ToString() As String
+        Return $"{dbname}: {dbkey}"
+    End Function
+
+End Class
+
+Public Class [date]
+
+    <XmlAttribute> Public Property type As String
+    <XmlAttribute> Public Property value As String
+
+    <MethodImpl(MethodImplOptions.AggressiveInlining)>
+    Public Overrides Function ToString() As String
+        Return $"{type}: {value}"
+    End Function
+
+End Class
+
+Public Class field
+
+    <XmlAttribute>
+    Public Property name As String
+
+    <XmlText> Public Property value As String
+
+    <MethodImpl(MethodImplOptions.AggressiveInlining)>
+    Public Overrides Function ToString() As String
+        Return $"{name}: {value}"
+    End Function
+
+End Class
+
