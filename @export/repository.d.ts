75ef71c1561827cbacb4f622b337d925e09020ad
guigang xie <xie.guigang@gcmodeller.org>
2023-12-11 09:57:26 +0800

    run package msbuild

---
+++ @export/repository.d.ts
@@ -0,0 +1,35 @@
+﻿// export R# package module type define for javascript/typescript language
+//
+//    imports "repository" from "MetaboLights";
+//
+// ref=MetaboLights.Rscript@MetaboLights, Version=1.0.0.0, Culture=neutral, PublicKeyToken=null
+
+/**
+*/
+declare namespace repository {
+   module as {
+      /**
+        * @param env default value Is ``null``.
+      */
+      function metaSet(database: any, env?: object): object;
+   }
+   /**
+     * @param env default value Is ``null``.
+   */
+   function experiments(database: any, env?: object): object;
+   /**
+     * @param ignoreCase default value Is ``true``.
+     * @param env default value Is ``null``.
+   */
+   function keywordFilters(studies: any, keywords: string, ignoreCase?: boolean, env?: object): any;
+   /**
+   */
+   function loadMetaEntries(file: string): object;
+   /**
+     * @param env default value Is ``null``.
+   */
+   function metabolites(database: any, env?: object): object;
+   /**
+   */
+   function parseChEBIEntity(xml: string): object;
+}
---
+++ man/MetaboLights/repository/repository.md
@@ -0,0 +1,10 @@
+# repository
+
+
+
++ [loadMetaEntries](repository/loadMetaEntries.1) 
++ [as.metaSet](repository/as.metaSet.1) 
++ [metabolites](repository/metabolites.1) 
++ [experiments](repository/experiments.1) 
++ [keywordFilters](repository/keywordFilters.1) 
++ [parseChEBIEntity](repository/parseChEBIEntity.1) 
