a582ae87acc429c9e1157cab81b15b2f636d1396
guigang xie <xie.guigang@gcmodeller.org>
2023-12-11 09:56:13 +0800

    run package msbuild

---
+++ @export/index.d.ts
@@ -0,0 +1,14 @@
+﻿// export R# source type define for javascript/typescript language
+//
+// package_source=MetaboLights
+
+declare namespace MetaboLights {
+   module _ {
+      /**
+      */
+      function onLoad(): object;
+   }
+   /**
+   */
+   function requestRepository(): object;
+}
