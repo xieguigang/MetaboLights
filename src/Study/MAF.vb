30e801266afa238421065c83004ab23d5a6ccfbc
この中二病に爆焔を！ <xie.guigang@gcmodeller.org>
2024-02-21 13:04:05 +0800

    make improvements of the annotation data model

--- src/Study/MAF.vb
+++ src/Study/MAF.vb
@@ -1,4 +1,6 @@
 ﻿
+Imports BioNovoGene.Analytical.MassSpectrometry.Math.Ms1.Annotations
+Imports BioNovoGene.BioDeep.Chemoinformatics.Formula
 Imports Microsoft.VisualBasic.ComponentModel.Collection.Generic
 Imports Microsoft.VisualBasic.ComponentModel.DataSourceModel
 Imports Microsoft.VisualBasic.Data.csv.StorageProvider.Reflection
@@ -6,12 +8,17 @@ Imports Microsoft.VisualBasic.Data.csv.StorageProvider.Reflection
 Public Class MAF : Inherits DynamicPropertyBase(Of Double)
     Implements INamedValue
     Implements IReadOnlyId
+    Implements ICompoundNameProvider, IExactMassProvider, IFormulaProvider
 
     <Column("database_identifier")> Public Property ID As String Implements INamedValue.Key, IReadOnlyId.Identity
-    <Column("chemical_formula")> Public Property formula As String
+    <Column("chemical_formula")> Public Property formula As String Implements IFormulaProvider.Formula
     <Column("smiles")> Public Property smiles As String
     <Column("inchi")> Public Property inchi As String
-    <Column("metabolite_identification")> Public Property metabolite_identification As String
+    ''' <summary>
+    ''' the metabolite name
+    ''' </summary>
+    ''' <returns></returns>
+    <Column("metabolite_identification")> Public Property metabolite_identification As String Implements ICompoundNameProvider.CommonName
     <Column("mass_to_charge")> Public Property mz As String
     <Column("fragmentation")> Public Property fragmentation As String
     <Column("modifications")> Public Property modifications As String
@@ -28,6 +35,12 @@ Public Class MAF : Inherits DynamicPropertyBase(Of Double)
     <Column("smallmolecule_abundance_sub")> Public Property smallmolecule_abundance_sub As String
     <Column("smallmolecule_abundance_stdev_sub")> Public Property smallmolecule_abundance_stdev_sub As String
     <Column("smallmolecule_abundance_std_error_sub")> Public Property smallmolecule_abundance_std_error_sub As String
+
+    Private ReadOnly Property ExactMass As Double Implements IExactMassProvider.ExactMass
+        Get
+            Return FormulaScanner.EvaluateExactMass(formula)
+        End Get
+    End Property
 
     Public Overrides Function ToString() As String
         Return metabolite_identification
