d2b773d92ebbce7959668d16b062d67e5d6ce989
この中二病に爆焔を！ <xie.guigang@gcmodeller.org>
2024-03-13 12:46:47 +0800

    fix for the json data model

--- src/Metabolon/Models/AssociationMatrix/responsesn.vb
+++ src/Metabolon/Models/AssociationMatrix/responsesn.vb
@@ -2,20 +2,21 @@
 
     Public Class responsesn
 
-        Public Property compid As  String
-        Public Property name": "glutamate",
-        Public Property superpathway": "Amino Acid",
-        Public Property subpathway": "Glutamate Metabolism",
-        Public Property platform": "LC/MS Pos Early",
-         Public Property chemid": 561,
-         Public Property ri": 1500,
-         Public Property mass": 148.0604,
-         Public Property cas": "56-86-0",
-         Public Property pubchem": "611",
-         Public Property chemspider": 591,
-        Public Property kegg": "C00025",
-        Public Property hmdb": "HMDB00148",
-       Public Property hmdb_ef
+        Public Property compid As String
+        Public Property name As String
+        Public Property superpathway As String
+        Public Property subpathway As String
+        Public Property platform As String
+        Public Property chemid As String
+        Public Property ri As Double
+        Public Property mass As Double
+        Public Property cas As String
+        Public Property pubchem As String
+        Public Property chemspider As String
+        Public Property kegg As String
+        Public Property hmdb As String
+        Public Property hmdb_ef As String
+
     End Class
 
 End Namespace
