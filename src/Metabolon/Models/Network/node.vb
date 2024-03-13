ab8d44517dc1bea3cd9b6d4239235749980306a9
この中二病に爆焔を！ <xie.guigang@gcmodeller.org>
2024-03-13 13:06:27 +0800

    node model

--- src/Metabolon/Models/Network/node.vb
+++ src/Metabolon/Models/Network/node.vb
@@ -2,6 +2,20 @@
 
     Public Class node
 
+        Public Property id As String
+        Public Property x As  Double
+        Public Property y As Double
+        Public Property label As String
+        Public Property compoundType As String
+        Public Property canonicalName As String
+        Public Property met_compid As String
+        Public Property ecNumber As String
+        Public Property title As String
+        Public Property color As String
+        Public Property size As Double
+        Public Property fixed As  Boolean
+        Public Property shape As String
+        Public Property chosen as  boolean 
 
     End Class
 End Namespace
--- src/Metabolon/Models/metabolon_network.vb
+++ src/Metabolon/Models/metabolon_network.vb
@@ -6,7 +6,12 @@ Namespace Metabolon.Models
 
         Public Property nodes As node()
         Public Property edges As edge()
+        Public Property options As [option]
+        Public Property width As Integer
+        Public Property height As Integer
 
+    End Class
 
+    Public Class [option]
     End Class
 End Namespace
