9c73c8111c24fd719e13b5f4adbd7439adf24135
この中二病に爆焔を！ <xie.guigang@gcmodeller.org>
2024-03-13 13:07:50 +0800

    add edge model

--- src/Metabolon/Models/Network/edge.vb
+++ src/Metabolon/Models/Network/edge.vb
@@ -2,5 +2,15 @@
 
     Public Class edge
 
+        Public Property from As String
+        Public Property [to] As  String
+        Public Property id As String
+        Public Property interaction As  String
+        Public Property direction As String
+        Public Property arrows As  String
+        Public Property title As String
+        Public Property smooth As  Boolean
+        Public Property color As String
+
     End Class
 End Namespace
