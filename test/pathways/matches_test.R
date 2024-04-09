0480a25a9251513c6aca5237819c71e5320d8339
この中二病に爆焔を！ <xie.guigang@gcmodeller.org>
2024-04-09 14:39:35 +0800

    run matches test siccess

--- test/pathways/matches_test.R
+++ test/pathways/matches_test.R
@@ -2,9 +2,17 @@ require(MetaboLights);
 
 let data_input = read.csv("D:\\metabolites.csv", row.names = 1, check.name = FALSE);
 
+rownames(data_input) = `metabo_${rownames(data_input)}`;
+
 print(data_input);
 
 let data = pathway_metabolites(data_input$cas);
 
+rownames(data) = rownames(data_input); 
+
 print(data);
+
+data = cbind(data_input, data);
+
+write.csv(data, file = "D:/metabolite_matches_cas.csv", row.names = TRUE);
 
