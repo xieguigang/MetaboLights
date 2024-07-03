2a6dd7e58237a327d7987e6fa4e6ae0d8aba3c2e
この中二病に爆焔を！ <xie.guigang@gcmodeller.org>
2024-07-03 08:45:06 +0800

    run package document updates

--- @export/MTBLSStudy.d.ts
+++ @export/MTBLSStudy.d.ts
@@ -16,6 +16,16 @@ declare namespace MTBLSStudy {
       function study_source(file: any, env?: object): object;
    }
    /**
+    * 
+    * > the sample group information is generates via the combination of 
+    * >  **`group`** and **`property`** data, 
+    * >  example as, there is data field named ``Factor Value[Cohort]`` in maf 
+    * >  table file, then the group parameter value should be ``factor`` and 
+    * >  the property parameter value should be ``Cohort``.
+    * 
+     * @param metadata -
+     * @param group the group source, value could be characteristics or factor
+     * @param property the group property data, value maybe various based on the study details
    */
    function sampleinfo(metadata: object, group: string, property: string): object;
 }
