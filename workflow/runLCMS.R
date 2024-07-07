26f55b6790d9fd3993cc9b585bfcea7cde38bef9
この中二病に爆焔を！ <xie.guigang@gcmodeller.org>
2024-07-07 13:28:04 +0800

    Update runLCMS.R

---
+++ workflow/runLCMS.R
@@ -0,0 +1,21 @@
+require(mzkit);
+
+[@info "the file path to the rawdata matrix file, should be a csv excel table file 
+        in format of features in rows and sample in columns."]
+let raw_data = ?"--rawdata" || stop("a rawdata matrix must be provided!");
+
+[@info "the file path to the sample information class data, this file should 
+        contains the data fields: 
+        
+        1. ID - the sample id, which will be used for reference to the sample data inside the expression matrix;	
+        2. injectionOrder - the injection order in the LCMS experiments, this information maybe usefull for data normalization;	
+        3. batch - the large panel batch number for the corresponding sample;	
+        4. sample_name - the display name of the samples;	
+        5. sample_info - the sample class or sample group name;	
+        6. color - color of the corresponding sample;	
+        7. shape - the shape name for draw the scatter of the sample data
+"]
+let sampleinfo = ?"--sampleinfo" || stop("the sampleinfo metadata must be provided!");
+
+[@info "a directory path for export the analysis result."]
+let workdir = ?"--export_dir" || file.path(dirname(raw_data), "LCMS_analysis");
