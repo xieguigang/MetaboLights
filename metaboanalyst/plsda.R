81c42341cd0478eb2252035ae155e128e9ddfdd2
この中二病に爆焔を！ <xie.guigang@gcmodeller.org>
2024-07-07 12:45:24 +0800

    plsda

---
+++ metaboanalyst/plsda.R
@@ -0,0 +1,35 @@
+plsda = function(x) {
+    # 载入所需的R包
+    library(ropls) # 用于PLS-DA分析
+    library(ggplot2) # 用于自定义绘图
+
+    # 假设你已经有了一个名为data的数据框，其中包含了你的代谢组学数据
+    # data <- read.csv("path_to_your_data.csv", row.names = 1)
+
+    # 由于iris数据集是连续的，我们需要将其转换为分类变量
+    data = as.data.frame(x);
+    data_class <- as.factor(data$class)
+
+    data[, "color"] = NULL;
+    data[, "sample_name"] = NULL;
+    data[, "class"] = NULL;
+
+    # 进行PLS-DA分析
+    # 首先，我们需要将数据转换为ropls所需的格式
+    data_pls <- opls(data = data, class = data_class, predI = 3)
+
+    # 提取PLS-DA得分
+    plsda_scores <- as.data.frame(data_pls@scores$X)
+
+    # 将PLS-DA得分和物种信息合并为一个数据框
+    plsda_data <- data.frame(class = data_class, plsda_scores)
+
+    # 使用ggplot2自定义绘图
+    ggplot(plsda_data, aes(x = Score1, y = Score2, color = class, group = class)) +
+        geom_point() +  # 绘制点
+        stat_ellipse(type = "norm", level = 0.95, geom = "polygon", fill = NA, alpha = 0.2) +  # 添加置信区间椭圆
+        theme_minimal() +  # 使用简洁主题
+        labs(color = 'class') +  # 添加图例标题
+        ggtitle("PLS-DA of Expression Data with Confidence Ellipses by Class") +  # 添加图表标题
+        theme(legend.position = "bottom")  # 将图例放在底部
+}
