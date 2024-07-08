ce1366be8aa1bc64e4a69784c28b7f39c9d5d1ce
この中二病に爆焔を！ <xie.guigang@gcmodeller.org>
2024-07-08 22:32:48 +0800

    Update anova.R

--- metaboanalyst/anova.R
+++ metaboanalyst/anova.R
@@ -1,18 +1,38 @@
 anova = function(x) {
-    class = unique(x$class);
-    aov_formula = as.formula(paste("response ~", paste(class, collapse = " * ")));
-    aov_model = aov(aov_formula, x);
+    class = x$class;
+
+    x[, "class"] = NULL;
+    x[,"color"] = NULL;
+    x[, "sample_name"] = NULL;
+
+    response = NULL;
+    group = NULL;
+    features = ncol(x);
+
+    for(i in 1:nrow(x)) {
+        response = append(response, unlist(x[i,,drop = TRUE]));
+        group = append(group, rep(class[i], features));
+    }
+
+    data = data.frame(
+        response = response,
+        group = as.factor(group)
+    );
+
+    aov_model = aov(response ~ group, data);
     aov_result = summary(aov_model);
     tukey_result = TukeyHSD(aov_model);
+    tukey_df = as.data.frame(tukey_result$group);
+    tukey_df$Comparison = factor(row.names(tukey_df))
 
     print(aov_result);
     print(tukey_result);
 
-    plot(tukey_result);
-
-    ggplot(data = as.data.frame(tukey_result$`factor1:factor2`), aes(x = Df, y = diff, color = p adj < 0.05)) +
-        geom_point() +
-        geom_errorbar(aes(ymin = lwr, ymax = upr), width = 0.2) +
-        theme_minimal() +
-        labs(x = "Group Comparison", y = "Mean Difference", color = "Significant")
+    ggplot(tukey_df, aes(x = Comparison, y = diff, color = `p adj` < 0.05)) +
+    geom_point() +
+    geom_errorbar(aes(ymin = lwr, ymax = upr), width = 0.2) +
+    theme_minimal() +
+    labs(x = "Group Comparison", y = "Mean Difference", color = "Significant") +
+    theme(axis.text.x = element_text(angle = 45, hjust = 1)) +
+    scale_color_manual(values = c("red", "black"), labels = c("Significant", "Not Significant"))
 }
