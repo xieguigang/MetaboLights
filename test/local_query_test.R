33fa5fc40549f08b50369df416bc175d11e082e6
この中二病に爆焔を！ <xie.guigang@gcmodeller.org>
2024-09-14 10:53:25 +0800

    configs for run test

---
+++ test/local_query_test.R
@@ -0,0 +1,11 @@
+require(MetaboLights);
+require(Darwinism);
+
+imports "repository" from "MetaboLights";
+imports "memory_query" from "Darwinism";
+
+setwd(@dir);
+
+let repo = loadMetaEntries("./eb_eye_metabolights_complete.xml") |> as.metaSet |> experiments();
+let pool = memory_query::load(repo);
+
